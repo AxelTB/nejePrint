08b8e3ffd609b57b1d94d3fe4f72b39678730315
Axxx <axel99999@hotmail.com>
2016-06-12 23:30:27 +0200

    With serial

--- convert.py
+++ convert.py
@@ -2,14 +2,40 @@ from PIL import Image
 import tkinter as tk
 from tkinter import filedialog
 
-root = tk.Tk()
-root.withdraw()
-file_path = filedialog.askopenfilename()
+import serial
+import time
 
-im = Image.open(file_path)
+ser = serial.Serial('/dev/ttyUSB0', 57600)
+ser.write(b"\xF6")
+rep=ser.read(2);
 
-im = im.resize((512,512), Image.ANTIALIAS)
-im = im.convert('1')
+print('Replied')
+print(rep)
+
+if rep == 'ep' :
+	root = tk.Tk()
+	root.withdraw()
+	file_path = filedialog.askopenfilename()
+
+	im = Image.open(file_path)
+
+	im = im.resize((512,512), Image.ANTIALIAS)
+	im = im.convert('1')
 
 
-im.save('converted.bmp')
+	im.save('converted.bmp')
+
+	ser.write(b"\xFE\xFE\xFE\xFE\xFE\xFE\xFE\xFE")
+	time.sleep(3)
+	ser.write(open("converted.bmp","rb").read())
+	time.sleep(3)
+	
+	ser.write(b"\xF3")
+	time.sleep(5)
+	
+	ser.write(b"\x10")
+	time.sleep(1)
+	
+	ser.write(b"\xF4")
+	
+	
