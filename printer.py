9e79cd64f31f67f2c7f4b8b58504e68e02c50af6
Axxx <axel99999@hotmail.com>
2017-09-24 18:28:03 +0200

    Timeout and retry serial handshake

--- printer.py
+++ printer.py
@@ -8,58 +8,65 @@ import glob,os
 os.chdir("/dev")
 choices=[]
 for file in glob.glob("ttyUSB*"):
-	choices.append(file)
+    choices.append(file)
 chosenSerial = choicebox("Which serial?", choices=choices)
 
-ser = serial.Serial(chosenSerial, 57600)
-ser.write(b"\xF6")
-rep=ser.read(2);
+#Create serial
+ser = serial.Serial(chosenSerial, 57600,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,3)
+#Set reponse to 0
+rep=0
 
-reply='bho'
-if rep == b'ep' :
-	choices = ["Convert Image","Load Converted Image","Preview","Print","Set Burning Time","Send Laser Home", "Reset Printer","Pause","Quit"]
+#Check printer online
+while rep != b'ep':
+    ser.write(b"\xF6")
+    rep=ser.read(2)
 
-	while (reply != 'Quit'):
-		reply = choicebox("What would you like to do?", choices=choices)
+    if (rep == b'ep'):
+        print('Printer connected')
+    else:
+        print('Printer not connected, trying again')
 
-		if reply == choices[0] :
+#Possible commands
+choices = ["Convert Image","Load Converted Image","Preview","Print","Set Burning Time","Send Laser Home", "Reset Printer","Pause","Quit"]
+reply='Bho'
+while (reply != 'Quit'):
+        reply = choicebox("What would you like to do?", choices=choices)
 
-			file_path = fileopenbox()
-			im = Image.open(file_path)
+        if reply == choices[0] :
 
-			im = im.resize((512,512), Image.NEAREST)
-			im = im.convert('1').transpose(Image.FLIP_TOP_BOTTOM)
+            file_path = fileopenbox()
+            im = Image.open(file_path)
+
+            im = im.resize((512,512), Image.NEAREST)
+            im = im.convert('1').transpose(Image.FLIP_TOP_BOTTOM)
 
 
-			im.save('converted.bmp')
-			msgbox("Check converted.bmp for a (Vertically flipped) preview")
-		elif reply == choices[1]:
-				print('Sending converted.bmp to machine, please wait')
-				ser.write(b"\xFE\xFE\xFE\xFE\xFE\xFE\xFE\xFE")
-				time.sleep(3)
-				print('.')
-				ser.write(open("converted.bmp","rb").read())
-				print('.')
-				time.sleep(3)
-				print('Done!')
+            im.save('converted.bmp')
+            msgbox("Check converted.bmp for a (Vertically flipped) preview")
+        elif reply == choices[1]:
+            print('Sending converted.bmp to machine, please wait')
+            ser.write(b"\xFE\xFE\xFE\xFE\xFE\xFE\xFE\xFE")
+            time.sleep(3)
+            print('.')
+            ser.write(open("converted.bmp","rb").read())
+            print('.')
+            time.sleep(3)
+            print('Done!')
 
-				ser.write(b"\xF3")
-		elif reply == choices[2]:
-			ser.write(b"\xF4")
-		elif reply == choices[3]:
-			ser.write(b"\xF1")
-		elif reply == choices[4]:
-			#burnTime=int(input("Enter burning time (1-240) : "))
-			burnTime=integerbox(msg='Enter Burning time', title='Set Burning Time', default=20, lowerbound=1, upperbound=240)
-		#ser.write(b"\x10")
-			if burnTime != None :
-				ser.write(bytes([burnTime]))
-		elif reply == choices[5]:
-			ser.write(b"\xF3")
-		elif reply == choices[6]:
-			ser.write(b"\xF9")
-		elif reply == choices[7]:
-			ser.write(b"\xF2")
-
-else :
-	print('Printer not connected')
+            ser.write(b"\xF3")
+        elif reply == choices[2]:
+            ser.write(b"\xF4")
+        elif reply == choices[3]:
+            ser.write(b"\xF1")
+        elif reply == choices[4]:
+            #burnTime=int(input("Enter burning time (1-240) : "))
+            burnTime=integerbox(msg='Enter Burning time', title='Set Burning Time', default=20, lowerbound=1, upperbound=240)
+            #ser.write(b"\x10")
+            if burnTime != None :
+                ser.write(bytes([burnTime]))
+        elif reply == choices[5]:
+            ser.write(b"\xF3")
+        elif reply == choices[6]:
+            ser.write(b"\xF9")
+        elif reply == choices[7]:
+            ser.write(b"\xF2")
